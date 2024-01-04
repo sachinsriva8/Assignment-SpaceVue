54555c5f29f701d8ab5463d62a7da55166f79c9a
sachin <sachin.sriva8@gmail.com>
2024-01-05 02:26:07 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Routing from "./components/routes/Route";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Routing />);
